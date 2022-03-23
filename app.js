7b98803992af373cea3e487c772f1f69f27098cc
KiV1 <zaki.jst@gmail.com>
2022-03-23 20:02:13 +0800

    initial

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+
+
+//run queries on the pool
+const pool = require("./db")
+
+app.use(express.json()) // -> req.body
+
+
+// ROUTES 
+
+//GET
+app.post("/auth/webhook", async(req,res)=>{
+    try {
+        const {id, name, email} = req.body.params;
+        const userData = await pool.query("SELECT * FROM auth_data where id=$1",[id])
+        if(userData.rowCount == 1){
+            res.json("ok");
+        }else{
+            await pool.query("INSERT INTO auth_data(id, name, email) VALUES ($1,$2,$3) RETURNING *",[id, name, email])
+            res.json("ok");
+        }
+
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+app.get('/auth/check', async(req,res)=>{
+    try {
+        const id = "";
+        const userData = await pool.query("SELECT * FROM auth_data where id=$1",[id])
+        res.json(userData.rowCount)
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+
+app.listen(8080, () => {
+    console.log('Server Started');
+});
---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE auth_database;
+
+-- create inventory table
+CREATE TABLE auth_data(
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255)
+);
